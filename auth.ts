d40ffed67fe7f8c49dffceb5105e26f93e108caf
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2024-01-05 20:19:04 +0400

    add rba A,U,C callbacks

--- auth.ts
+++ auth.ts
@@ -1,12 +1,18 @@
+
+
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { UserRole } from "@prisma/client";
 
 import { db } from "./lib/db"
+import { getUserById } from '@/data/user';
 
 
 
 import authConfig
     from "@/auth.config"
+
+
 
 export const {
     handlers: { GET, POST },
@@ -14,6 +20,33 @@ export const {
     signIn,
     signOut,
 } = NextAuth({
+    callbacks: {
+
+        async session({ token, session }) {
+            // console.log({
+            //     SessionToken: token,
+            // });
+
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+
+            }
+
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole;
+            }
+            return session
+        },
+        async jwt({ token }) {
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+
+            if (!existingUser) return token
+            token.role = existingUser.role;
+            return token
+        }
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
     ...authConfig,
