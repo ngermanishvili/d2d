09319fcc1bf237064315d0d8105f36e964cb1323
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2024-01-14 19:05:06 +0400

    add types inside couriers id page in page.tsx

--- app/(protected)/couriers/[id]/page.tsx
+++ app/(protected)/couriers/[id]/page.tsx
@@ -1,17 +1,28 @@
-import {db} from "@/lib/db";
-import {UserForm} from "../components/user-form";
+import { db } from "@/lib/db";
+import { UserForm } from "../components/user-form";
 
-const UserEditPage = async ({params}: {params: {id: string}}) => {
+const UserEditPage = async ({ params }: { params: { id: string } }) => {
   const user = await db.user.findUnique({
-    where: {id: params.id},
+    where: { id: params.id },
   });
 
-  if (!user) return {error: "User ar moidzebna"};
+  if (!user) return { error: "User ar moidzebna" };
 
   return (
     <div className="flex-col ">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        {user && <UserForm initialData={user} />}
+        {user && (
+          <UserForm
+            initialData={{
+              ...user,
+              name: user.name || undefined,
+              number: user.number || undefined,
+              image: user.image || undefined,
+              role: user.role || undefined,
+              email: user.email || undefined,
+            }}
+          />
+        )}
       </div>
     </div>
   );
