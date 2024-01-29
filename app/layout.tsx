91f103124c43f6b0b2a3048be3b8868e1841e57b
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2024-01-29 15:49:10 +0400

    sa

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { SessionProvider } from 'next-auth/react';
 import { auth } from '@/auth';
-import Head from 'next/head';
 import './globals.css';
 import NProgressWrapper from '@/components/ui/nprogress-bar';
 import Footer from '@/components/ui/footer';
@@ -30,16 +29,7 @@ export default async function RootLayout({
   return (
     <>
       <SessionProvider session={session}>
-        <Head>
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta name="robots" content="index, follow" />
-          {metadata.title && <title>{metadata.title.toString()}</title>}
-          <meta name="description" content={metadata.description || ''} />
-          <meta name="keywords" content={Array.isArray(metadata?.keywords) ? metadata.keywords.join(', ') : metadata?.keywords || ''} />
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-          <link rel="shortcut icon" href="/favicon.ico" type="image/facebook-icon" />
-        </Head>
+
         <NProgressWrapper />
         <div className="bg-slate-50">
           <Header isSession={isSession} />
