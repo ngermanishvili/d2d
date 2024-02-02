2a11c0cb2fbf5cb2307e978cb8dfb4241db2f858
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2024-02-02 16:07:11 +0400

    Remove unused URLs from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -25,14 +25,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             changeFrequency: "weekly",
             priority: 0.8,
         },
-        {
-            url: "https://d2d.ge/about",
-            priority: 0.7,
-        },
-        {
-            url: "https://d2d.ge/services",
-            priority: 0.6,
-        },
+
         ...(await generateBlogPostsSitemapObjects()).map((o) => ({
             url: `https://d2d.ge/blogposts/${o.slug}`,
             lastModified: o.updatedAt,
