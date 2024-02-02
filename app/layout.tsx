48e1d9eec65f949f64b421d8ad74f462048a9827
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2024-02-02 14:55:21 +0400

    Update Facebook Messenger Chat code

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,27 +43,25 @@ export default async function RootLayout({
               {/* Facebook Messenger Chat code */}
               <Script id="fb-chat" strategy="lazyOnload">
                 {`
-                  // Messenger საუბრის დანამატი Code
-                  var chatbox = document.getElementById('fb-customer-chat');
-                  chatbox.setAttribute("page_id", "115850238226188");
-                  chatbox.setAttribute("attribution", "biz_inbox");
+              var chatbox = document.getElementById('fb-customer-chat');
+              chatbox.setAttribute("page_id", "115850238226188");
+              chatbox.setAttribute("attribution", "biz_inbox");
 
-                  // Your SDK code
-                  window.fbAsyncInit = function() {
-                    FB.init({
-                      xfbml: true,
-                      version: 'v19.0'
-                    });
-                  };
+              window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml: true,
+                  version: 'v18.0'
+                });
+              };
 
-                  (function(d, s, id) {
-                    var js, fjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id)) return;
-                    js = d.createElement(s); js.id = id;
-                    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-                    fjs.parentNode.insertBefore(js, fjs);
-                  }(document, 'script', 'facebook-jssdk'));
-                `}
+              (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));
+              `}
               </Script>
               {/* Tawk.to Chat code */}
               {/* Tawk.to Chat code */}
