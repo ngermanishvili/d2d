de342359a75fb3731bf4b50350f8f3e66461b4c0
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2024-02-02 19:33:13 +0400

    Add about page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -25,6 +25,11 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             changeFrequency: "weekly",
             priority: 0.8,
         },
+        {
+            url: "https://d2d.ge/about",
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
 
         ...(await generateBlogPostsSitemapObjects()).map((o) => ({
             url: `https://d2d.ge/blogposts/${o.slug}`,
