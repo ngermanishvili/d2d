60c26d4158640726c214725c1efd2c8c18ce8638
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2024-02-04 18:44:50 +0400

    Add input fields to CouriersPage

--- app/(protected)/couriers/page.tsx
+++ app/(protected)/couriers/page.tsx
@@ -30,6 +30,15 @@ const CouriersPage = () => {
     number: item.number || "",
     image: item.image || "",
     role: item.role,
+    input1: item.input1 || "",
+    input2: item.input2 || "",
+    input3: item.input3 || "",
+    input4: item.input4 || "",
+    input5: item.input5 || "",
+    input6: item.input6 || "",
+    input7: item.input7 || "",
+    input8: item.input8 || "",
+
   }));
 
   return (
