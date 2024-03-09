aa33025ec5f5c0823ad09afbbe584f78c8a2d8b9
samxaradzenika <110243055+samxaradzenika@users.noreply.github.com>
2024-03-09 17:13:30 +0400

    sidescroll

--- app/api/couriers/route.ts
+++ app/api/couriers/route.ts
@@ -4,7 +4,26 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   try {
-    const users = await db.user.findMany({});
+    const users = await db.user.findMany({
+      where: {
+        role: UserRole.USER 
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        input1: true,
+        input2: true,
+        input3: true,
+        input4: true,
+        input5: true,
+        input6: true,
+        input7: true,
+        input8: true,
+        userType: true,
+      },
+    });
 
     return NextResponse.json(users);
   } catch (error) {
