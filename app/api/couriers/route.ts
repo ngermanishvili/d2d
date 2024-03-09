67ab0c69b9b9a94a2bd4e5b028b40aba6ba645bc
samxaradzenika <110243055+samxaradzenika@users.noreply.github.com>
2024-03-09 18:38:55 +0400

    aa

--- app/api/couriers/route.ts
+++ app/api/couriers/route.ts
@@ -4,13 +4,13 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   try {
-    const users = await db.user.findMany({
-      where: {
-        role: "COURIER" || "USER",
-      },
-    });
+    // const users = await db.user.findMany({
+    //   where: {
+    //     role: "COURIER" || "USER",
+    //   },
+    // });
 
-    return NextResponse.json(users);
+    return NextResponse.json(await db.user.findMany());
   } catch (error) {
     console.log("[SHIPMENT_GET]", error);
     return new NextResponse("Internal error BROJ", { status: 500 });
